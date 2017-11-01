eb9cdb1fcfe9fb5adcf44516deb490d09b91ea1b
王昱智 <429373960@qq.com>
2017-11-01 11:11:26 +0800

    Update loadtext.h

--- loadtext.h
+++ loadtext.h
@@ -1,4 +1,11 @@
- ifstream in("readme.txt", ios::in);
- istreambuf_iterator<char> beg(in), end;
- string strdata(beg, end);
- in.close();
+#include <fstream>
+#include <string>
+#include <iterator>
+inline std::string LoadFileContent_Binary(const char*path)
+{
+	std::ifstream in("readme.txt", std::ios::in| std::ios::binary);
+	std::istreambuf_iterator<char> beg(in), end;
+	std::string strdata(beg, end);
+	in.close();
+	return strdata;
+}
